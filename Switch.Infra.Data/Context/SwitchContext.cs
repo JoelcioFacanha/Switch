ac849af484e1ab7c9365ba78d3edc97df30147f0
Joélcio Façanha <joelciomoreira@gmail.com>
2020-06-06 13:43:20 -0300

    Adicionando o contexto da aplicação

---
+++ Switch.Infra.Data/Context/SwitchContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+
+namespace Switch.Infra.Data.Context
+{
+    public class SwitchContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public SwitchContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
