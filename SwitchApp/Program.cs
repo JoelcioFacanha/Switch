6a9ac74622f9d625ad905e75bef4a6eef9c9ddee
Joélcio Façanha <joelciomoreira@gmail.com>
2020-06-22 13:21:08 -0300

    Inserindo um objeto no banco de dados MySql

---
+++ SwitchApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+using Switch.Infra.Data.Context;
+using System;
+
+namespace SwitchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new Usuario()
+            {
+                Nome = "Usuario1",
+                SobreNome = "SobreNomeUsuario",
+                Email = "usuario@gmail.com",
+                DataNascimento = DateTime.Parse("30/06/1977"),
+                Senha = "123456",
+                UrlFoto = "fotos/foto1.png"
+            };
+
+            var optionsBuilder = new DbContextOptionsBuilder<SwitchContext>();
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseMySql("Server=localhost;Database=switch;Uid=root;Pwd=jofamo/*2019", m => m.MigrationsAssembly(nameof(Switch.Infra.Data)));
+
+            using (var context = new SwitchContext(optionsBuilder.Options))
+            {
+                context.Usuarios.Add(user);
+                context.SaveChanges();
+            }
+        }
+    }
+}
